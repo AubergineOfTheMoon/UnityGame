baa29c5098196e2636f7214b6306c52f933dd8aa
evan <eakers1123@gmail.com>
2017-01-31 15:49:08 -0500

    just my project

---
+++ RPG/RPG/Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract public class Player : MonoBehaviour
+{
+    int speed;
+
+    public abstract Vector3 moveVector(Input Vector3); 
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
---
+++ RPG/RPG/Assets/Scripts/Player1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1 : Player {
+    public override Vector3 moveVector(Input Vector3)
+    {
+        //main return Vector3 = new Vector3(this.Vector3);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
