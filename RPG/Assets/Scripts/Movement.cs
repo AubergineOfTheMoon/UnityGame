ef47c3031646688c732c64bedc446eeffbba446c
Omega571 <ruegsejm@mail.uc.edu>
2017-01-31 18:39:38 -0500

    PLayer Controls Update

---
+++ RPG/Assets/Scripts/Movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract public class Movement : MonoBehaviour
+{
+    
+    public Vector3 moveVector;
+
+    public float GetHorizontalInput()
+    {
+        return Input.GetAxis("Horizontal");
+    }
+
+    public float GetVerticalInput()
+    {
+        return Input.GetAxis("Vertical");
+    }
+
+    abstract public void Move();   
+
+
+}
---
+++ RPG/Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Movement
+{
+    public float speed = 5f;
+
+   
+
+    public override void Move()
+    {
+        // Get Input
+        moveVector.x = GetHorizontalInput() * speed * Time.deltaTime;        
+        moveVector.z = GetVerticalInput() * speed * Time.deltaTime;
+        //Move
+        transform.Translate(moveVector);
+    }
+
+
+    
+
+    // Use this for initialization DO NOT INITIALIZE IN UPDATE
+    void Start ()
+    {
+        //Saves us 1/60(ish) of a second at the start to streamline the startup (rather than setting the y component in the updatde funct.
+        moveVector.y = 0;
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Move();
+        
+    }
+
+    
+    
+}
+
+
