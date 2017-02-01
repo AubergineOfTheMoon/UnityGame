6082d309ad0f4cf36a76953c6283fdd946fcb5cf
evan <eakers1123@gmail.com>
2017-01-31 20:45:00 -0500

    Added Tile script and basic tiles

---
+++ RPG/Assets/Scripts/Tile.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour {
+
+    static public float tileLength;
+    bool unitAndPlayerOnTile;
+
+    // Use this for initialization
+    void Start()
+    {
+        tileLength = transform.localScale.x;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider triggerCollider)
+    {
+        if (triggerCollider.tag == "Unit" && triggerCollider.tag == "Player") unitAndPlayerOnTile = true; //pending Ryan and Caden
+        else unitAndPlayerOnTile = false;
+    }
+
+}
